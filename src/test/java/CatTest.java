d87a26d433d7620ba14172b99c5deb32acb9892e
abdullaaaah <abdullaaaah_@outlook.com>
2021-09-27 15:45:26 -0400

    Fixes the errors

--- src/test/java/CatTest.java
+++ src/test/java/CatTest.java
@@ -15,4 +15,7 @@ public class CatTest {
     public void TestSound() {
         assertEquals("Meow!", c.sound());
     }
+
+    @Test(timeout = 50)
+    public void TestGetPrice() { assertEquals(1000, c.getPrice());}
 }
